ea14f6274993f2873baf781e57fd3c9ef7db2ad8
aac-devs <aac.devs@gmail.com>
2021-02-26 08:55:19 -0300

    folder structure created, db connection done, server started

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../database/config");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    this.paths = {};
+
+    // Connect DB
+    this.connectDB();
+
+    // Middlewares
+    this.middlewares();
+
+    // Routes
+    this.routes();
+  }
+
+  async connectDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+
+    // Reading and parse body
+    this.app.use(express.json());
+
+    // Public folder
+    this.app.use(express.static("public"));
+  }
+
+  routes() {}
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server running al http://localhost:${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
